cd20c4174383b6ddc747682ca35eb460175e447c
CJ Van Huben <cvanhube@u.rochester.edu>
2017-10-03 00:26:17 -0400

    Changed content in Frankie's php

--- frankieportfolio.php
+++ frankieportfolio.php
@@ -25,15 +25,33 @@
 				<article>
 					    <h2>Who am I?</h2>
     
-					    <p>I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.</p>
+					    <p>I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.
+					    </p>
 					    <h2>Work Experience</h2>
     
-					    I've worked at non profit organizations to help minimize the minority opportunity gap. I like to help students apply for college,and apply themselves in college as well. I hope to create my own not for profit to continue to help minority students in inner cities, succeed in ways I have and have not.
-					    <h2>Skills</h2>
+					    
+					    // <h2>Skills</h2>
     
-					    <p>Basic programming skills in Python and JavaScript; light web design proficiency
-					    Proficient in Microsoft Office: Word, Excel, PowerPoint.
-					    Fluent in English with basic proficiency of Spanish</p>
+					    // <p>Basic programming skills in Python and JavaScript; light web design proficiency
+					    // Proficient in Microsoft Office: Word, Excel, PowerPoint.
+					    // Fluent in English with basic proficiency of Spanish</p>
+
+					    <h2>Current Classes</h2>
+					    <ul>
+					    <li>CSC 174</li>
+					    <li>REL 316W</li>
+					    <li>ENG 249</li>
+					    <li>ENG 131</li>
+					    </ul>
+
+					    <h2>Work Experience</h2>
+					    <p>I've worked at non profit organizations to help minimize the minority opportunity gap. I like to help students apply for college,and apply themselves in college as well. I hope to create my own not for profit to continue to help minority students in inner cities, succeed in ways I have and have not.
+					    </p>
+
+					    <h2>Hobbies</h2>
+					    <p> I enjoy helping people, reading, and makeup. </p>
+
+
 				</article>
 			</div>
 		</div>
