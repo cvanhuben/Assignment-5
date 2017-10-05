2c03920d9f15f3181fd287644fa2364e182c002f
CJ Van Huben <cvanhube@u.rochester.edu>
2017-10-05 14:26:10 -0400

    fixed some of the allignment

--- frankieportfolio.php
+++ frankieportfolio.php
@@ -14,12 +14,13 @@
 						<img src="images/financialecon.jpeg">
 						<figcaption>Frankie has a love for analytics</figcaption>
 					</figure>
+					<div class = "whoAmI">
 					    <h2>Who am I?</h2>
     
 					   		 <p>
 					    		I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.
 					   		 </p>
-					    
+					    </div>
 					    <h2>Current Classes</h2>
 					    	<ul>
 					    		<li>CSC 174</li>
@@ -28,15 +29,18 @@
 					    		<li>ENG 131</li>
 					    	</ul>
 
+							<div class = "workExperience>
 					    <h2>Work Experience</h2>
 					    	<p>
 					    		I've worked at non profit organizations to help minimize the minority opportunity gap. I like to help students apply for college,and apply themselves in college as well. I hope to create my own not for profit to continue to help minority students in inner cities, succeed in ways I have and have not.
 					    	</p>
-
+					    	</div>
+					    <div class = "hobbies">
 					    <h2>Hobbies</h2>
 					    	<p> 
 					    		I enjoy helping people, reading, and makeup.
 					    	</p>
+					    	</div>
 				</article>
 			</div>
 		</div>
--- jien.php
+++ jien.php
@@ -17,12 +17,13 @@
 						<img src="images/Jien.jpg">
 						<figcaption>Jien killing it on the vocals</figcaption>
 				</figure>
-					<div class="about">
+					<div class = "whoAmI">
 						<h2>Who Am I?</h2>
 							<p>
 								I was born to a Japanese mother (Naomi) and an American father (Josh) on 29th October, 1996 in Tokyo, Japan. Shortly before my first birthday, my mother relocated permanently to America to give me the opportunity to be raised with my father and give me access to America's higher education system. I attended Colonie Central High School 2010-2014, and I am currently a senior at the University of Rochester.
 							</p>
-					</div>
+							</div>
+					
 					<div class="more">
 
 					<h2>Current Classes</h2>
@@ -33,16 +34,19 @@
 							<li>JPN 294</li>
 						</ul>
 
+					<div class = "workExperience">
 					<h2>Work Experience</h2>
 						<p>
 							I worked at Barnes & Noble in Union Square, and I currently work at Barnes & Noble in College Town. I also used to be an office assistant.
 						</p>
+						</div>
 
-
+						<div class = "hobbies">
 						<h2>Hobbies</h2>
 							<p>
 								I like to do things like roll dice, read, play piano, suffer through existential crises, suffer in general, and code my own character sheets.
 							</p>
+							</div>
 					</div>
 				</article>
 			</div>
